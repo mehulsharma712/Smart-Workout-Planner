d33236520532ee803dd854fb60820f49514b0d45
mehulsharma712 <mehulsharma712@gmail.com>
2026-01-11 12:25:24 +0530

    .

---
+++ backend/index.js
@@ -0,0 +1,103 @@
+const express = require("express")
+const path = require("path");
+const axios = require("axios")
+const app = express()
+
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine","ejs");
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+
+app.get("/",(req,res)=>{
+    res.render("form")
+})
+
+app.post("/recommend", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:5000/predict",
+      {
+        goal: req.body.goal,
+        experience: req.body.experience,
+        days_per_week: Number(req.body.days_per_week),
+        time_per_session: Number(req.body.time_per_session),
+      }
+    );
+
+    const WorkoutPlans={
+      full_body :{
+        title:"Full Body Workout",
+        day:[
+          {
+            day:"Day 1",
+            exercises:["Squats", "Push Ups", "Pull Ups", "Plank"]
+          },
+          {
+          day:"Day 2",
+          exercises: ["Deadlift", "Bench Press", "Lat Pulldown", "Crunches"]
+          }
+
+        ]
+      },
+      upper_lower: {
+        title: "Upper / Lower Split",
+        days: [
+      {
+        day: "Upper Body",
+        exercises: ["Bench Press", "Shoulder Press", "Pull Ups", "Biceps Curl"]
+      },
+      {
+        day: "Lower Body",
+        exercises: ["Squats", "Leg Press", "Hamstring Curl", "Calf Raises"]
+      }
+    ]
+  },
+      push_pull_legs: {
+        title: "Push Pull Legs Split",
+        days: [
+      {
+        day: "Push",
+        exercises: ["Bench Press", "Overhead Press","Incline-dumbell", "Triceps Dips"]
+      },
+      {
+        day: "Pull",
+        exercises: ["Pull Ups", "Barbell Row", "Biceps Curl","Hammer","Generator"]
+      },
+      {
+        day: "Legs",
+        exercises: ["Squats", "Lunges", "Leg Curl", "Calf Raises"]
+      }
+    ]
+  },
+      bro_split: {
+    title: "Bro Split",
+    days: [
+      { day: "Chest", exercises: ["Bench Press", "Incline DB Press"] },
+      { day: "Back", exercises: ["Deadlift", "Lat Pulldown"] },
+      { day: "Legs", exercises: ["Squats", "Leg Press"] },
+      { day: "Shoulders", exercises: ["Overhead Press", "Lateral Raises"] },
+      { day: "Arms", exercises: ["Biceps Curl", "Triceps Pushdown"] }
+    ]
+  }
+      
+    };
+
+    const predictedSplit = response.data.predicted_split;
+    const Workout =WorkoutPlans[predictedSplit]
+
+    res.render("result", { split: predictedSplit,Workout });
+
+  } catch (error) {
+    console.error(error.message);
+    res.send("Error communicating with ML service");
+  }
+});
+
+
+app.listen(3000,()=>{
+    console.log('Node server running on port 3000')
+})
---
+++ ml/api.py
@@ -0,0 +1,54 @@
+import warnings
+warnings.filterwarnings("ignore")
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Load trained model and encoders
+model = joblib.load("model.pkl")
+goal_encoder = joblib.load("goal_encoder.pkl")
+experience_encoder = joblib.load("experience_encoder.pkl")
+target_encoder = joblib.load("target_encoder.pkl")
+
+@app.route("/")
+def home():
+    return "Smart Workout ML API is running"
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Read JSON data from request
+    data = request.get_json()
+
+    # Extract input features
+    goal = data["goal"]
+    experience = data["experience"]
+    days = data["days_per_week"]
+    time = data["time_per_session"]
+
+    # Create DataFrame (same format as training)
+    input_df = pd.DataFrame([{
+        "goal": goal,
+        "experience": experience,
+        "days_per_week": days,
+        "time_per_session": time
+    }])
+
+    # Encode categorical features
+    input_df["goal"] = goal_encoder.transform(input_df["goal"])
+    input_df["experience"] = experience_encoder.transform(input_df["experience"])
+
+    # Make prediction
+    prediction_encoded = model.predict(input_df)
+    prediction = target_encoder.inverse_transform(prediction_encoded)
+
+    # Return result
+    return jsonify({
+        "predicted_split": prediction[0]
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
